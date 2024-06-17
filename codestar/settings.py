9af8d6bd13a156ff13d4b65c22bcfac34ae79ab2
willcodinggod white <walidwhite@live.co.uk>
2024-06-17 15:15:58 +0000

    built first post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--@j!4@gyqdeg8hz&1&ebt#yta0gskic_3*kc)syldznmvhc)-w'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgres://ufzsgyp19rb:AUCgnlylFjFI@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/hefty_fact_given_477885" )
+
+os.environ.setdefault("SECRET_KEY", "walid1985")
