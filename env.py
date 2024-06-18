4335b3a60f3246657c5253efdf6329b30820f3eb
willcodinggod white <walidwhite@live.co.uk>
2024-06-18 15:23:05 +0000

    enable serving of image files

--- env.py
+++ env.py
@@ -4,3 +4,6 @@ os.environ.setdefault(
     "DATABASE_URL", "postgres://ufzsgyp19rb:AUCgnlylFjFI@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/hefty_fact_given_477885" )
 
 os.environ.setdefault("SECRET_KEY", "walid1985")
+
+os.environ.setdefault(
+    "CLOUDINARY_URL","cloudinary://745124679143235:GSl2q265on-d0tUC7UUaHbvRg0o@dpujkdryq")
