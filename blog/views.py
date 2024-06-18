1e1415be3e67c825da5d73e149f4bfddde61483c
willcodinggod white <walidwhite@live.co.uk>
2024-06-18 14:52:09 +0000

    added comment CRUD

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
-from .models import Post
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
+from .forms import CommentForm
 
 # Create your views here.
-
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
-
-
 
 def post_detail(request, slug):
     """
@@ -25,13 +25,84 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+    comment_form = CommentForm()
 
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post,
-         "coder": "Matt Rudge"},
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form
+        },
     )
+
+def comment_edit(request, slug, comment_id):
+    """
+    View to edit comments
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if request.user != comment.author:
+        messages.error(request, "You do not have permission to edit this comment.")
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST, instance=comment)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False  # Reset approval status after edit
+            comment.save()
+            messages.success(request, "Comment updated successfully.")
+            return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+        else:
+            messages.error(request, "Error updating comment.")
+
+    else:
+        comment_form = CommentForm(instance=comment)
+
+    return render(
+        request,
+        "blog/comment_edit.html",
+        {
+            "form": comment_form,
+            "post": post,
+            "comment": comment
+        },
+    )
+
+def comment_delete(request, slug, comment_id):
+    """
+    View to delete comments
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
