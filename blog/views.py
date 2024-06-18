ac0638a4c6ae89586366ca84d121f3b9b57e0788
willcodinggod white <walidwhite@live.co.uk>
2024-06-18 11:26:04 +0000

    Add About app

--- blog/views.py
+++ blog/views.py
@@ -32,5 +32,6 @@ def post_detail(request, slug):
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {"post": post,
+         "coder": "Matt Rudge"},
     )
